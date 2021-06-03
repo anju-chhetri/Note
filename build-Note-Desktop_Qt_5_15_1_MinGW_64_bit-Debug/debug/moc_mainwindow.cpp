30dafd9cfa381877b0cc9a26690e3f7828035ff4
Anju Chhetri <57993069+anju-chhetri@users.noreply.github.com>
2021-06-03 23:24:58 +0545

    windeploy folder added after richtext problem

--- build-Note-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-Note-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[21];
-    char stringdata0[233];
+    QByteArrayData data[18];
+    char stringdata0[203];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -41,24 +41,20 @@ QT_MOC_LITERAL(5, 51, 4), // "HTML"
 QT_MOC_LITERAL(6, 56, 9), // "plainText"
 QT_MOC_LITERAL(7, 66, 17), // "close_application"
 QT_MOC_LITERAL(8, 84, 5), // "about"
-QT_MOC_LITERAL(9, 90, 9), // "copy_text"
-QT_MOC_LITERAL(10, 100, 8), // "cut_text"
-QT_MOC_LITERAL(11, 109, 10), // "paste_text"
-QT_MOC_LITERAL(12, 120, 9), // "redo_text"
-QT_MOC_LITERAL(13, 130, 9), // "undo_text"
-QT_MOC_LITERAL(14, 140, 9), // "save_file"
-QT_MOC_LITERAL(15, 150, 13), // "check_save_as"
-QT_MOC_LITERAL(16, 164, 10), // "check_open"
-QT_MOC_LITERAL(17, 175, 8), // "new_file"
-QT_MOC_LITERAL(18, 184, 11), // "change_font"
-QT_MOC_LITERAL(19, 196, 19), // "call_increase_count"
-QT_MOC_LITERAL(20, 216, 16) // "call_other_class"
+QT_MOC_LITERAL(9, 90, 9), // "redo_text"
+QT_MOC_LITERAL(10, 100, 9), // "undo_text"
+QT_MOC_LITERAL(11, 110, 9), // "save_file"
+QT_MOC_LITERAL(12, 120, 13), // "check_save_as"
+QT_MOC_LITERAL(13, 134, 10), // "check_open"
+QT_MOC_LITERAL(14, 145, 8), // "new_file"
+QT_MOC_LITERAL(15, 154, 11), // "change_font"
+QT_MOC_LITERAL(16, 166, 19), // "call_increase_count"
+QT_MOC_LITERAL(17, 186, 16) // "call_other_class"
 
     },
     "MainWindow\0get_input_cmd\0\0return_richText\0"
     "MarkDown\0HTML\0plainText\0close_application\0"
-    "about\0copy_text\0cut_text\0paste_text\0"
-    "redo_text\0undo_text\0save_file\0"
+    "about\0redo_text\0undo_text\0save_file\0"
     "check_save_as\0check_open\0new_file\0"
     "change_font\0call_increase_count\0"
     "call_other_class"
@@ -71,7 +67,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      19,   14, // methods
+      16,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -79,30 +75,24 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  109,    2, 0x0a /* Public */,
-       3,    0,  110,    2, 0x0a /* Public */,
-       4,    0,  111,    2, 0x0a /* Public */,
-       5,    0,  112,    2, 0x0a /* Public */,
-       6,    0,  113,    2, 0x0a /* Public */,
-       7,    0,  114,    2, 0x0a /* Public */,
-       8,    0,  115,    2, 0x0a /* Public */,
-       9,    0,  116,    2, 0x0a /* Public */,
-      10,    0,  117,    2, 0x0a /* Public */,
-      11,    0,  118,    2, 0x0a /* Public */,
-      12,    0,  119,    2, 0x0a /* Public */,
-      13,    0,  120,    2, 0x0a /* Public */,
-      14,    0,  121,    2, 0x0a /* Public */,
-      15,    0,  122,    2, 0x0a /* Public */,
-      16,    0,  123,    2, 0x0a /* Public */,
-      17,    0,  124,    2, 0x0a /* Public */,
-      18,    0,  125,    2, 0x0a /* Public */,
-      19,    0,  126,    2, 0x0a /* Public */,
-      20,    0,  127,    2, 0x0a /* Public */,
+       1,    0,   94,    2, 0x0a /* Public */,
+       3,    0,   95,    2, 0x0a /* Public */,
+       4,    0,   96,    2, 0x0a /* Public */,
+       5,    0,   97,    2, 0x0a /* Public */,
+       6,    0,   98,    2, 0x0a /* Public */,
+       7,    0,   99,    2, 0x0a /* Public */,
+       8,    0,  100,    2, 0x0a /* Public */,
+       9,    0,  101,    2, 0x0a /* Public */,
+      10,    0,  102,    2, 0x0a /* Public */,
+      11,    0,  103,    2, 0x0a /* Public */,
+      12,    0,  104,    2, 0x0a /* Public */,
+      13,    0,  105,    2, 0x0a /* Public */,
+      14,    0,  106,    2, 0x0a /* Public */,
+      15,    0,  107,    2, 0x0a /* Public */,
+      16,    0,  108,    2, 0x0a /* Public */,
+      17,    0,  109,    2, 0x0a /* Public */,
 
  // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -136,18 +126,15 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 4: _t->plainText(); break;
         case 5: _t->close_application(); break;
         case 6: _t->about(); break;
-        case 7: _t->copy_text(); break;
-        case 8: _t->cut_text(); break;
-        case 9: _t->paste_text(); break;
-        case 10: _t->redo_text(); break;
-        case 11: _t->undo_text(); break;
-        case 12: _t->save_file(); break;
-        case 13: _t->check_save_as(); break;
-        case 14: _t->check_open(); break;
-        case 15: _t->new_file(); break;
-        case 16: _t->change_font(); break;
-        case 17: _t->call_increase_count(); break;
-        case 18: _t->call_other_class(); break;
+        case 7: _t->redo_text(); break;
+        case 8: _t->undo_text(); break;
+        case 9: _t->save_file(); break;
+        case 10: _t->check_save_as(); break;
+        case 11: _t->check_open(); break;
+        case 12: _t->new_file(); break;
+        case 13: _t->change_font(); break;
+        case 14: _t->call_increase_count(); break;
+        case 15: _t->call_other_class(); break;
         default: ;
         }
     }
@@ -185,13 +172,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 19)
+        if (_id < 16)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 19;
+        _id -= 16;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 19)
+        if (_id < 16)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 19;
+        _id -= 16;
     }
     return _id;
 }
